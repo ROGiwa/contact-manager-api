0d74684b42700e600167eb4fa70d17db78539f92
giwa <giwarilwan00@gmail.com>
2025-01-15 03:48:16 +0100

    continuous comit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const dotenv = require('dotenv')
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+
+app.use('/api/contacts', require('./routes/contactRoutes'))
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
